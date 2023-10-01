dd0c22e2fb85bffc0544126ec6d06e345daf669f
Jacek Ziolek <jziolek@x-one.com.pl>
2023-10-01 18:17:06 +0200

    added htmx to project dependencies and added base.html template

--- project/config/urls.py
+++ project/config/urls.py
@@ -16,7 +16,9 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from app.views import TestView
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('', TestView.as_view(), name='test'),
 ]
