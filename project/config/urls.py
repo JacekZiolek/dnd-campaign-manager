70e587814408f62d0fc1e86b6c4aa2b02c5d9851
Jack Herby <JacekZiolek@proton.me>
2024-01-05 00:49:46 +0100

    feat(account): add profile picure to user account WIP

--- project/config/urls.py
+++ project/config/urls.py
@@ -1,3 +1,5 @@
+from django.conf import settings
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import path, include
 from django.views.generic import TemplateView
@@ -35,3 +37,7 @@ urlpatterns = [
         path('profile/', profile.ProfileView.as_view(), name='profile'),
     ])),
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
