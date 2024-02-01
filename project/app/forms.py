78942292e2ce41d8ba5a52dff602b5e605a592a7
Jack Herby <JacekZiolek@proton.me>
2024-02-01 22:34:28 +0100

    feat: fix help text styles on singup form passowrd1 field

--- project/app/forms.py
+++ project/app/forms.py
@@ -1,38 +1,9 @@
 from django.contrib.auth.forms import UserCreationForm
-from django.db import transaction
 
-from .models import DungeonMaster, Player, User
+from .models import User
 
 
 class SignUpForm(UserCreationForm):
     class Meta(UserCreationForm.Meta):
         model = User
         fields = ("username", "email", "password1", "password2")
-
-
-class PlayerSignUpForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = User
-        fields = ("username", "email", "password1", "password2")
-
-    @transaction.atomic
-    def save(self):
-        user = super().save(commit=False)
-        user.is_player = True
-        user.save()
-        Player.objects.create(user=user)
-        return user
-
-
-class DungeonMasterSignUpForm(UserCreationForm):
-    class Meta(UserCreationForm.Meta):
-        model = User
-        fields = ("username", "email", "password1", "password2")
-
-    @transaction.atomic
-    def save(self):
-        user = super().save(commit=False)
-        user.is_dungeon_master = True
-        user.save()
-        DungeonMaster.objects.create(user=user)
-        return user
