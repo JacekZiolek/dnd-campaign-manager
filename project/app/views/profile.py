805308e0828e35d18a699881350b6089680078a0
JacekZ <47577862+JacekZiolek@users.noreply.github.com>
2024-02-02 22:19:25 +0100

    Revert "Feature bugfixes and refactor after adding tailwind"

--- project/app/views/profile.py
+++ project/app/views/profile.py
@@ -1,9 +1,9 @@
-from django.contrib.auth.mixins import UserPassesTestMixin
 from django.views.generic import TemplateView
+from django.contrib.auth.mixins import UserPassesTestMixin
 
 
 class ProfileView(UserPassesTestMixin, TemplateView):
-    template_name = "profile.html"
+    template_name = 'profile.html'
 
     def test_func(self):
         return self.request.user.is_authenticated
